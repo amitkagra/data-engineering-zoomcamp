809e69a10d8e1e63de4ea64d19a7877be5540f31
amitkagra <36561444+amitkagra@users.noreply.github.com>
2025-02-27 10:31:11 +0530

    hw files

---
+++ 04-analytics-engineering/fct_fhv_monthly_zone_traveltime_p90.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with trip_duration_calculated as (
+    select
+        *,
+        timestamp_diff(dropOff_datetime, pickup_datetime, second) as trip_duration
+    from {{ ref('dim_fhv_trips') }}
+)
+
+select 
+
+    *,
+    PERCENTILE_CONT(trip_duration, 0.90) 
+    OVER (PARTITION BY year, month, PUlocationID, DOlocationID) AS trip_duration_p90
+
+
+from trip_duration_calculated
+
+--dbt build --select +fct_fhv_monthly_zone_traveltime_p90.sql+ --vars '{is_test_run: false}'
---
+++ 04-analytics-engineering/stg_fhv_2019.sql
@@ -0,0 +1,30 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with tripdata as 
+(
+  select *
+  from {{ source('staging','FHV_2019') }}
+  where dispatching_base_num is not null 
+)
+select
+
+    dispatching_base_num,
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropOff_datetime as timestamp) as dropOff_datetime,
+    PUlocationID,
+    DOlocationID,
+    SR_Flag,
+    Affiliated_base_number
+    
+from tripdata
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
